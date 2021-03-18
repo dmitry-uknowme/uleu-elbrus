5553da93992d9b9369ef8a12248fe218af080439
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-03-19 03:09:37 +0500

    quiz modal

--- src/js/modules/quiz.js
+++ src/js/modules/quiz.js
@@ -1,14 +1,25 @@
 export default () => {
 	let currentStage = 1;
-	const maxStage = 6;
+	const maxStage = 7;
 	const quizStage = document.querySelectorAll('.quiz__stage');
 	const nextBtn = document.querySelector('.quiz-box__btn');
+	const quizTitle = document.querySelector('.quiz__title');
 	const quizAnswers = document.querySelectorAll('.quiz__answer');
+	const quizBox = document.querySelector('.quiz__box');
+	const quizSuccess = document.querySelector('.quiz__success');
 	quizStage[currentStage - 1].classList.add('_active');
+
+	const modal = document.querySelector('modal');
 
 	const toNextStage = () => {
 		if (currentStage < maxStage) {
 			currentStage++;
+		}
+		if (currentStage >= maxStage) {
+			quizBox.classList.add('_modal');
+			quizSuccess.classList.add('_active');
+			nextBtn.style.opacity = '0';
+			quizTitle.style.opacity = '0';
 		}
 		const activeStage = document.querySelector('.quiz__stage._active');
 		if (activeStage) {
