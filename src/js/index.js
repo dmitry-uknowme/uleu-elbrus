68a4e27db7d18085fe7e350b447a4e367c5c39c8
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-03-18 22:22:32 +0500

    quiz functional

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/index.sass';
+
+import quiz from './modules/quiz';
+
+document.addEventListener('DOMContentLoaded', () => {
+	quiz();
+});
