a58852449cf2a5822a7247bdccf09da0f21cb34b
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-03-20 18:21:24 +0500

    modal window

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.sass';
+import bindModals from './modules/bindModals';
 import houseMaterials from './modules/houseMaterials';
 import quiz from './modules/quiz';
 
 document.addEventListener('DOMContentLoaded', () => {
+	bindModals();
 	quiz();
 	houseMaterials();
 });
