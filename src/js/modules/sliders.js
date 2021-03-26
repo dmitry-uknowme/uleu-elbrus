913beac4e37464e33f308cb86d2831ddb13d0185
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-03-26 20:01:15 +0500

    adaptive for modal window

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -6,7 +6,7 @@ Swiper.use([Navigation]);
 export const quizSlider = new Swiper('.quiz__container', {
 	wrapperClass: 'quiz__wrapper',
 	slideClass: 'quiz__stage',
-	// allowTouchMove: false,
+	allowTouchMove: false,
 	navigation: {
 		nextEl: '.quiz__btn',
 		prevEl: '.swiper-button-prev',
